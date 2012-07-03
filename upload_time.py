d5db7ea21e1e63fed360f85ef6bd3202adca13a9
Vladimir Grichina <vgrichina@componentix.com>
2012-07-04 00:22:40 +0300

    Filtering by description pattern and start date

--- upload_time.py
+++ upload_time.py
@@ -1,7 +1,11 @@
 import csv
+import re
+from datetime import datetime
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S +0000"
 
 def parse_time(csvFile):
-	reader = csv.reader(open(csvFile, "r"))
+	reader = csv.reader(open(csvFile, "r"), skipinitialspace = True)
 	i = 0
 	for row in reader:
 		if i == 2:
@@ -13,5 +17,12 @@ def parse_time(csvFile):
 		i += 1
 
 
-for t in parse_time("time.txt"):
+def filter_time(timeSeq, pattern, startTime):
+	for entry in timeSeq:
+		if re.match(pattern, entry["Description"]):
+			whenTime = datetime.strptime(entry["When"], DATE_FORMAT)	
+			if whenTime >= startTime:
+				yield entry
+
+for t in filter_time(parse_time("time.txt"), ".*Reader.*", datetime.strptime("2012-05-30 13:59:58 +0000", DATE_FORMAT)):
 	print t
